d23b8da180dd938303bfdf8ba222412010615c01
JongWoo Park <jjongwoo.dev@gmail.com>
2020-03-13 21:38:58 +0900

    algospot: re-try JUMPGAME.cpp

--- algospot/JUMPGAME.cpp
+++ algospot/JUMPGAME.cpp
@@ -13,7 +13,16 @@ bool jump(int y, int x){
     int jS = board[y][x];
     return jump(y+jS, x) || jump(y, x+jS);
 }
+
+bool myJump(int y, int x){
+    if(y >= n || x>= n) return false;
+    if(y == n-1 && x == n-1) return true;
+    int js = board[y][x];
+    return myJump(y+js, x) || myJump(y, x+js)
+}
+
 int cache[100][100];
+
 int jump2(int y, int x){
     if(y >= n || x >= n) return 0;
     if(y == n-1 && x == n-1) return 1;
@@ -22,6 +31,16 @@ int jump2(int y, int x){
     if(ret != -1) return ret;
     int js = board[y][x];
     ret = (jump2(y+js, x) || jump2(y, x+js));
+    return ret;
+}
+
+int jump2(int y, int x){
+    if(y >= n || x >= n ) return 0;
+    if(y == n-1 && x == n-1) return 1;
+    int& ret = cache[y][x];
+    if(ret != -1) return ret;
+    int js = board[y][x];
+    ret= (jump2(y+js, x) || jump2(y, x+js));
     return ret;
 }
 
