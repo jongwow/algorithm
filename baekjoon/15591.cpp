79ee16a01f1161df9701f7ef2a4eae5dacf48cc7
JongWoo Park <jjongwoo.dev@gmail.com>
2023-01-06 23:18:32 +0900

    WIP

--- baekjoon/15591.cpp
+++ baekjoon/15591.cpp
@@ -15,63 +15,84 @@ int path[5001][5001];
 int ks[5001];
 int videoIndexes[5001];
 
-
-int main(){
+int main()
+{
     freopen("./15591.txt", "r", stdin);
     int N, Q;
     cin >> N >> Q;
-    
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             cache[i][j] = INF;
         }
     }
-    for(int i = 0; i < N-1; i++){
+    for (int i = 0; i < N - 1; i++)
+    {
         int start, end, value;
         cin >> start >> end >> value;
-        path[start-1][end-1] = value;
-        path[end-1][start-1] = value;
+        path[start - 1][end - 1] = value;
+        path[end - 1][start - 1] = value;
     }
     int maxK = 0;
-    
-    for(int i = 0; i < Q; i++){
+
+    for (int i = 0; i < Q; i++)
+    {
         int k, videoIndex;
         cin >> k >> videoIndex;
         ks[i] = k;
-        if(maxK < k){
+        if (maxK < k)
+        {
             maxK = k;
         }
-        videoIndexes[i] = videoIndex;
+        videoIndexes[i] = videoIndex - 1;
     }
     //=== start
-    for(int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++)
+    {
         int root = i;
         queue<int> q;
         q.push(root);
         visited[root] = true;
-        while(!q.empty()){
+        while (!q.empty())
+        {
             int current = q.front();
             q.pop();
             vector<int> adj;
-            for(int near = 0; near < N; near++){
-                if(path[])
-                adj.push_back(near)
+            // adjacent nodes
+            for (int near = 0; near < N; near++)
+            {
+                if (path[current][near] != 0 && !visited[near])
+                {
+                    adj.push_back(near);
+                }
+            }
+            for (int adjIndex = 0; adjIndex < adj.size(); adjIndex++)
+            {
+                cache[root][adj[adjIndex]] = min(cache[root][current])]]
+
+                // cache[current][adj[j]] = min(cache[current][adj[j]], path[adj[j]][current]);
             }
 
-            for(int j = 0; j < N; j++){
-                if(!visited[j]){
-                    if(path[current][j] != 0){
+            for (int j = 0; j < N; j++)
+            {
+                if (!visited[j])
+                {
+                    if (path[current][j] != 0)
+                    {
                         cache[current][j]
                     }
                 }
-                
             }
         }
     }
     //=== end
     cout << "map" << endl;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             cout << path[i][j] << " ";
         }
         cout << endl;
