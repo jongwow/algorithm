7c09e65554cbb50b56bee5f1c72c568d6e2dc7bf
JongWoo Park <jjongwoo.dev@gmail.com>
2023-01-21 22:04:19 +0900

    backjoon: refactor 17780

--- baekjoon/17780.js
+++ baekjoon/17780.js
@@ -4,7 +4,7 @@ const COLOR = {
     WHITE: 0,
     WALL: 4,
 };
-const MAX_COUNT = 1000;
+const MAX_COUNT = 10;
 
 class Pos {
     constructor(x, y, n){
@@ -46,13 +46,16 @@ class Dir {
         this.y = -this.y;
     }
 }
+
 class OldHorse{
     constructor(index, position, direction){
         this.index = index;
         this.pos = position;
         this.dir = direction;
         this.movable = true;
+        this.mounts = 1;
         this.nextHorse = undefined;
+        this.lastHorse = undefined;
     }
     nextMove(firstMove = true){
         let nextPosition = new Pos(this.pos.x + this.dir.x, this.pos.y + this.dir.y, this.pos.n)
@@ -77,6 +80,7 @@ class OldHorse{
         }
         belowHorse.nextHorse = this;
     }
+
     _moveNextBlue(){
        this.dir.reverse();
        this.nextMove(false);
@@ -120,6 +124,70 @@ class OldHorse{
         return count;
     }
 }
+class Horse{
+    constructor(index, position, direction){
+        this.index = index;
+        this.pos = position;
+        this.dir = direction;
+        this.movable = true;
+        this.mounts = 1;
+        this.nextHorse = undefined;
+        this.lastHorse = undefined;
+    }
+    nextMove(firstMove = true){
+        let nextPosition = new Pos(this.pos.x + this.dir.x, this.pos.y + this.dir.y, this.pos.n)
+        if(nextPosition.IsOutOfWall()){
+            firstMove === true && this._moveNextBlue();
+        } else {
+            const nextTile = map[nextPosition.x][nextPosition.y];
+            if(nextTile === COLOR.BLUE){
+                firstMove === true && this._moveNextBlue();
+            } else if(nextTile === COLOR.RED){
+                // console.log(`${this.index} move to red (${nextPosition.x}, ${nextPosition.y})`)
+                this._moveNextRed(nextPosition);
+            } else if(nextTile === COLOR.WHITE){
+                // console.log(`${this.index} move to white (${nextPosition.x}, ${nextPosition.y})`)
+                this._moveNextWhite(nextPosition);
+            }
+        }
+    }
+    Mount(bottom){
+        // console.log(`${this.index}(${this.mounts}) mount on ${bottom.index}(${bottom.mounts})`)
+        this.movable = false;
+        bottom.mounts += this.mounts
+        bottom.lastHorse = this;
+    }
+
+    _moveNextBlue(){
+        this.dir.reverse();
+        this.nextMove(false);
+     }
+    _moveNextRed(nextPosition){
+        this.reverseHorses(nextPosition);
+    }
+    _moveNextWhite(nextPos){
+        this.pos = nextPos;
+        if(this.lastHorse){
+            this.lastHorse.pos = nextPos;
+        }
+    }
+    //굳이 다 reverse할 필요가 있을까.
+    reverseHorses(nextPosition){
+        let currentHorse = this;
+        currentHorse.pos = nextPosition;
+        let lastHorse = currentHorse.lastHorse;
+        if(lastHorse){
+            lastHorse.pos = currentHorse.pos;
+            currentHorse.movable = false;
+            lastHorse.movable = true;
+            lastHorse.lastHorse = currentHorse;
+            currentHorse.lastHorse = undefined;
+        }
+    }
+    countHorses(){
+        return this.mounts;
+    }
+}
 const map = [];
 const occupied = [];
 const horses = [];
@@ -132,7 +200,6 @@ function processInput(lines){
     }
     for(let i = N+1; i < N+1+K; i++){
         const [x, y, dir]= lines[i].split(" ").map((item) => +item);
-        // horses.push(new Horse(x, y, dir));
         horses.push(new OldHorse(i - N, new Pos(x-1, y-1, N), new Dir(dir)));
     }
     for(let i = 0; i < horses.length; i++){
@@ -145,7 +212,7 @@ function processInput(lines){
 function solve(lines){
     processInput(lines);
     let stepCount = -1;
-    
+
     while(stepCount++ < MAX_COUNT){
         for(let i = 0; i < horses.length; i++){
             const count = horses[i].countHorses();
@@ -153,7 +220,7 @@ function solve(lines){
                 return stepCount;
             }
         }
-        
+        // console.log(occupied);
         for(let i = 0; i < horses.length; i++){
             const currentHorse = horses[i];
             if(currentHorse.movable === false){
@@ -166,11 +233,7 @@ function solve(lines){
             x = currentHorse.pos.x;
             y = currentHorse.pos.y;
             if(occupied[x][y]){
-                const count = currentHorse.Mount(horses[occupied[x][y]]);
-                console.log(count)
-                // if(count >= 4){
-                //     return stepCount;
-                // }
+                currentHorse.Mount(horses[occupied[x][y]]);
             } else{
                 occupied[x][y] = i;
             }
@@ -180,24 +243,92 @@ function solve(lines){
 }
 
 
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./17780.txt";
-let inputs = fs.readFileSync(filePath).toString().split("\n");
-// const inputs = [
-//     '6 10',        '0 1 2 0 1 1',
-//     '1 2 0 1 1 0', '2 1 0 1 1 0',
-//     '1 0 1 1 0 2', '2 0 1 2 0 1',
-//     '0 2 1 0 2 1', '1 1 1',
-//     '2 2 2',       '3 3 4',
-//     '4 4 1',       '5 5 3',
-//     '6 6 2',       '1 6 3',
-//     '6 1 2',       '2 4 3',
-//     '4 2 1'
-//   ]
-console.log(inputs);
-const result = solve(inputs);
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./17780.txt";
+// let inputs = fs.readFileSync(filePath).toString().split("\n");
+
+const testCases = [
+    [
+    '4 4',     
+    '0 0 2 0',
+    '0 0 1 0', 
+    '0 0 1 2',
+    '0 2 0 0', 
+    '2 1 1',
+    '3 2 3',   '2 2 1',
+    '4 1 2'
+],
+    [
+        '4 4',     '0 0 0 0',
+        '0 0 0 0', '0 0 0 0',
+        '0 0 0 0', '1 1 1',
+        '1 2 1',   '1 3 1',
+        '1 4 1'
+      ],
+      [
+        '4 4',     '0 0 0 0',
+        '0 0 0 0', '0 0 0 0',
+        '0 0 0 0', '1 1 1',
+        '1 2 1',   '1 3 1',
+        '2 4 3'
+      ],
+      [
+        '4 4',     '0 0 0 0',
+        '0 0 0 0', '0 0 0 0',
+        '0 0 0 0', '1 1 1',
+        '1 2 1',   '1 3 1',
+        '3 3 3'
+      ],
+      [
+        '4 4',     '0 0 2 0',
+        '0 0 1 0', '0 0 1 2',
+        '0 2 0 0', '2 1 1',
+        '3 2 3',   '2 2 1',
+        '4 1 3'
+      ],
+      [
+        '6 10',        '0 1 2 0 1 1',
+        '1 2 0 1 1 0', '2 1 0 1 1 0',
+        '1 0 1 1 0 2', '2 0 1 2 0 1',
+        '0 2 1 0 2 1', '1 1 1',
+        '2 2 2',       '3 3 4',
+        '4 4 1',       '5 5 3',
+        '6 6 2',       '1 6 3',
+        '6 1 2',       '2 4 3',
+        '4 2 1'
+      ],
+      ['4 4', 
+      '1 0 1 0', 
+      '1 0 1 0',
+      '0 1 1 0', 
+      '0 1 1 0',
+      '1 1 1', '1 2 1', '1 4 3', '4 1 3'
+    ],
+    [
+        '4 4',     '0 0 0 0',
+        '0 0 0 0', '0 0 0 0',
+        '0 0 0 0', '1 1 2',
+        '1 2 2',   '1 3 2',
+        '1 4 2'
+      ],
+]
+const answers = [
+    -1,
+    1,
+    1,
+    2,
+    8,
+    9,
+    -1,
+    3,
+]
+
+let tcNum = 7;
+console.log(testCases[tcNum]);
+const result = solve(testCases[tcNum]);
 if(result > MAX_COUNT){
     console.log(-1);
 } else {
     console.log(result)
 }
+console.log(answers[tcNum]);
